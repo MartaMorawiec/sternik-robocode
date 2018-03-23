0558ca42b6d67e6dd10e51db42b05c4cde9b5eea
Marta Morawiec <marta.morawiec@edu.uekat.pl>
2018-03-23 22:15:35 +0100

    travis 2

--- src/pl/sternik/mm/robocode/Muffin.java
+++ src/pl/sternik/mm/robocode/Muffin.java
@@ -1,7 +1,28 @@
 package pl.sternik.mm.robocode;
 
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
 
 public class Muffin extends Robot {
+    public void run(){
+        setBodyColor(Color.PINK);
+        setGunColor(Color.WHITE);
+        setRadarColor(Color.RED);
 
+        setScanColor(Color.YELLOW);
+        setBulletColor(Color.ORANGE);
+
+        while(true){
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e){
+        fire(1);
+    }
 }
